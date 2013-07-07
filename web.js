b377dd476bf08e4c0d19cb886b41bd608f9ff508
Jose Costa <jpa1costa@gmail.com>
2013-07-07 20:03:22 +0000

    web.js reading vs1

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var raw_buffer;
+fs.readFile('/myrepo/bitstarter/index.html', function (err, data) {
+    if(err) throw err;
+    raw_buffer = data;
+});
+
+var content = raw_buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
